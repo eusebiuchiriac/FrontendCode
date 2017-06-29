edf97e0116a0d6ff142b79b7fa4702ad4ccfc1b2
eusebiu.chiriac <eusebiu.chiriac@yahoo.com>
2017-06-29 15:21:26 +0300

    29.06.2017 Done everything

--- Day 3/scripts/controllers/demo/MathController.js
+++ Day 3/scripts/controllers/demo/MathController.js
@@ -1,7 +1,16 @@
-hrApp.controller('MathController', ['$scope', function($scope){
-    
+hrApp.controller('MathController', ['$scope','MathService', function($scope, MathService){
+
+    $scope.nmba = 0;
+    $scope.nmbb = 0;
+
+    $scope.MathService = MathService;
+
     $scope.calculate = function() {
 //        TODO #8 calculate op1, op2, op3, op4
+           $scope.op1 = $scope.MathService.add($scope.nmba,$scope.nmbb);
+           $scope.op2 = $scope.MathService.substract($scope.nmba,$scope.nmbb);
+           $scope.op3 = $scope.MathService.multiply($scope.nmba,$scope.nmbb);
+           $scope.op4 = $scope.MathService.divide($scope.nmba,$scope.nmbb);
 
 //        TODO #13 refactor your calculations using MathService
     }
